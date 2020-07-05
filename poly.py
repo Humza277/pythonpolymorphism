88a76b2374f59d39959f55b838a0a1e3e267effe
hmalak <hmalak@spartaglobal.com>
2020-07-05 23:57:29 +0100

    polycode

---
+++ poly.py
@@ -0,0 +1,12 @@
+num1 = 1
+num2 = 2
+num3 = 3
+print(num1 + num2 + num3)
+
+str1 = "Python"
+str2 = "Learning"
+print(str1 + " " + str2)
+
+print(len("RacingCar"))
+print(len(["COBAL", "Ruby", "Fortran"]))
+print(len({"Name": "Humza", "City": "London"}))
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Polymorphism
+Polymorphism is a very important concept in programming. It refers to the use of a single type entity (method, operator or object) to represent different types in different scenarios.
