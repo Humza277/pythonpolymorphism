e4984ebd0b27fc0c903822ac12bff5c14329da62
hmalak <hmalak@spartaglobal.com>
2020-07-06 08:51:12 +0100

    Comments

--- poly.py
+++ poly.py
@@ -5,8 +5,11 @@ print(num1 + num2 + num3)
 
 str1 = "Python"
 str2 = "Learning"
-print(str1 + " " + str2)
+print(str1 + " " + str2) # can concatenate two variables together
 
+# different versions of len, printing out differing lengths
+# #according the the datatype that is used
 print(len("RacingCar"))
 print(len(["COBAL", "Ruby", "Fortran"]))
 print(len({"Name": "Humza", "City": "London"}))
+
